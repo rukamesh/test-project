5d4693f92c46934c2de3c9a26918d5919b0734e1
rukamesh <rukamesh.kumar@talentica.com>
2016-05-10 19:07:50 +0530

    testing

--- unit_test.py
+++ unit_test.py
@@ -2,8 +2,8 @@ import unittest
 from primes import is_prime
 
 class PrimesTestCase(unittest.TestCase):
-        def test_is_seven_prime(self):
-                self.assertTrue(is_prime(7))
+        def test_is_eleven_prime(self):
+                self.assertTrue(is_prime(11))
 
 if __name__ =='__main__':
         unittest.main()
